0b894761a3f6925fb75b016ee33f645d5487d1d5
johnpospisil <audiofreak7@gmail.com>
2019-07-10 07:43:20 -0700

    Connect the Field and Button to the Store

--- src/components/Button.js
+++ src/components/Button.js
@@ -5,13 +5,12 @@ import ColorContext from "../contexts/ColorContext";
 class Button extends React.Component {
   render() {
     // console.log(this.context);
-    // const text = this.context === 'english' ? 'Sumit' : 'Választ';
     return (
       <ColorContext.Consumer>
         {(color) => (
           <button className={`ui button ${color}`}>
             <LanguageContext.Consumer>
-              {value => (value === "english" ? "Submit" : "Választ")}
+              {({ language }) => (language === "english" ? "Submit" : "Választ")}
             </LanguageContext.Consumer>
           </button>
         )}
--- src/components/Field.js
+++ src/components/Field.js
@@ -5,7 +5,7 @@ class Field extends React.Component {
   static contextType = LanguageContext;
 
   render() {
-    const text = this.context === 'english' ? 'Name' : 'Név';
+    const text = this.context.language === 'english' ? 'Name' : 'Név';
 
     return (
       <div className="label ui">
